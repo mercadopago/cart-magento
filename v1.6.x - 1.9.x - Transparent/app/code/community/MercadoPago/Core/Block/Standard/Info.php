3c843a8b69370a25b453be9bb12e3e2c0c008d50
Damian Achaga <dachaga@summasolutions.net>
2015-09-28 14:10:39 -0300

    see #65 remove setmodulename() in info blocks

--- v1.6.x - 1.9.x - Transparent/app/code/community/MercadoPago/Core/Block/Standard/Info.php
+++ v1.6.x - 1.9.x - Transparent/app/code/community/MercadoPago/Core/Block/Standard/Info.php
@@ -22,7 +22,6 @@ class MercadoPago_Core_Block_Standard_Info
     {
         parent::_construct();
         $this->setTemplate('mercadopago/standard/info.phtml');
-        $this->setModuleName('Mage_Payment');
     }
 
 

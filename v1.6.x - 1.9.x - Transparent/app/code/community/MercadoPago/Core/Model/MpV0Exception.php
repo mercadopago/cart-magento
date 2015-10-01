fabbe61a90ebc501aaef58b9b06cbf657e9fada2
Damian Achaga <dachaga@summasolutions.net>
2015-10-01 10:56:33 -0300

    see #67 map errors messages v0

--- v1.6.x - 1.9.x - Transparent/app/code/community/MercadoPago/Core/Model/MpV0Exception.php
+++ v1.6.x - 1.9.x - Transparent/app/code/community/MercadoPago/Core/Model/MpV0Exception.php
@@ -7,23 +7,13 @@ class MercadoPago_Core_Model_MpV0Exception extends MercadoPago_Core_Model_MpExce
 {
     protected $messagesMap =
         array(
-            205 => 'Enter the card number',
-            208 => 'Enter the card expiration month',
-            209 => 'Enter the card expiration year',
-            212 => 'Enter the document type',
-            213 => 'Enter the document subtype',
-            214 => 'Enter the document number',
-            220 => 'Enter the bank',
-            221 => 'Enter the card holder name',
-            224 => 'Enter the security code',
-            'E301'=>'Card number is invalid.',
-            'E302'=>'Security code is invalid.',
-            316=>'Card Holder Name is invalid.',
-            322=>'Document Type is invalid.',
-            323=>'Document Sub Type is invalid.',
-            324=>'Document Number is invalid.',
-            325=>'Month is invalid.',
-            326=>'Year is invalid.',
+            106 => 'Cannot operate between users from different countries.',
+            109 => 'Payment method does not process installments.',
+            129 => 'Cannot pay this amount with this paymentMethod.',
+            150 => 'You user cannot do payments currently',
+            151 => 'You user cannot do payments currently with this payment method.',
+            204 => 'Unavailable paymentmethod currently.',
+            801 => 'Already posted the same request in the last minute.',
             'campaign_code_doesnt_match' => "Doesn't find a campaign with the given code."
         );
 

399ba7b1aa91d922da2f6e22d2d3790cb034186b
Damian Achaga <dachaga@summasolutions.net>
2015-10-02 14:00:54 -0300

    remove require_once from files

--- v1.6.x - 1.9.x - Transparent/app/code/community/MercadoPago/Core/Model/Standard/Payment.php
+++ v1.6.x - 1.9.x - Transparent/app/code/community/MercadoPago/Core/Model/Standard/Payment.php
@@ -14,8 +14,6 @@
 * @license    	http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 */
 
-require_once(Mage::getBaseDir('lib') . '/mercadopago/mercadopago.php');
-
 class MercadoPago_Core_Model_Standard_Payment extends Mage_Payment_Model_Method_Abstract
 {
     //configura o lugar do arquivo para listar meios de pagamento
@@ -46,7 +44,7 @@ class MercadoPago_Core_Model_Standard_Payment extends Mage_Payment_Model_Method_
         //seta sdk php mercadopago
         $client_id = Mage::getStoreConfig('payment/mercadopago/client_id');
         $client_secret = Mage::getStoreConfig('payment/mercadopago/client_secret');
-        $mp = new MP($client_id, $client_secret);
+        $mp = new MercadoPago_Lib_Api($client_id, $client_secret);
         
         //monta a prefernecia
         $pref = $this->makePreference();

e296fe92bcc0ef61e70c5902cbed004f371ed979
Damian Achaga <dachaga@summasolutions.net>
2015-10-08 12:39:28 -0300

    see #100 add translations

--- v1.6.x - 1.9.x - Transparent/app/code/community/MercadoPago/Core/Model/Api/V1/Exception.php
+++ v1.6.x - 1.9.x - Transparent/app/code/community/MercadoPago/Core/Model/Api/V1/Exception.php
@@ -10,13 +10,13 @@ class MercadoPago_Core_Model_Api_V1_Exception
         array(
             1000 => 'The items Quantity has exceeded the limits. We could not process your payment.',
             2001 => 'Already posted the same request in the last minute.',
-            2017 => 'The transaction amount is invalid, We could not process your payment.',
+            2017 => 'The transaction amount is invalid. We could not process your payment.',
             2020 => 'You user cannot do payments currently',
             2022 => 'You have exceeded the max number of refunds for this payment.',
             2024 => 'Payment too old to be refunded',
             2034 => 'You user cannot do payments currently',
             2039 => 'Coupon code is invalid',
-            2040 => 'Your user e-mail does not exist, please check data and retry later',
+            2040 => 'Your user e-mail does not exist. Please check data and retry later',
             2043 => 'Coupon code is invalid',
             3000 => 'You must provide your cardholder name with your card data',
             3011 => 'Not found payment method or credit card. Please check the form data and retry.',
@@ -28,13 +28,13 @@ class MercadoPago_Core_Model_Api_V1_Exception
             3018 => 'Expiration month can not be empty. Please check the form data and retry.',
             3019 => 'Expiration year can not be empty. Please check the form data and retry.',
             3020 => 'Cardholder name can not empty. Please check the form data and retry.',
-            3021 => 'Cardholder documentnumber can not be empty. Please check the form data and retry.',
+            3021 => 'Cardholder document number can not be empty. Please check the form data and retry.',
             3022 => 'Cardholder document type can not be empty. Please check the form data and retry.',
             3023 => 'Cardholder document subtype can not be empty. Please check the form data and retry.',
             3029 => 'Expiration month is invalid. Please check the form data and retry.',
             3030 => 'Expiration year is invalid. Please check the form data and retry.',
-            4004 => 'Installments atributte can not be empty, Please check the form data and retry.',
-            4026 => 'Coupon_amount is invalid, Please check the form data and retry.',
+            4004 => 'Installments attribute can not be empty. Please check the form data and retry.',
+            4026 => 'Coupon amount is invalid. Please check the form data and retry.',
             'campaign_code_doesnt_match' => "Doesn't find a campaign with the given code."
         );
 

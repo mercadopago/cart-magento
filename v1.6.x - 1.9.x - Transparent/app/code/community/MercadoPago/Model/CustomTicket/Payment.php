bdc5084eabad863b148f26864deb2e9cfcfd73a4
vagrant <vagrant@precise32.(none)>
2015-12-09 11:51:13 +0000

    ajuste coupon de desconto para OSC

--- v1.6.x - 1.9.x - Transparent/app/code/community/MercadoPago/Model/CustomTicket/Payment.php
+++ v1.6.x - 1.9.x - Transparent/app/code/community/MercadoPago/Model/CustomTicket/Payment.php
@@ -17,11 +17,11 @@
 require_once(Mage::getBaseDir('lib') . '/mercadopago/mercadopago.php');
 
 class MercadoPago_Model_CustomTicket_Payment extends Mage_Payment_Model_Method_Abstract{
-    
+
     //configura o lugar do arquivo para listar meios de pagamento
     protected $_formBlockType = 'mercadopago/customticket_form';
     protected $_infoBlockType = 'mercadopago/customticket_info';
-    
+
     protected $_code = 'mercadopago_customticket';
 
     protected $_canSaveCc = false;
@@ -38,26 +38,26 @@ class MercadoPago_Model_CustomTicket_Payment extends Mage_Payment_Model_Method_A
     public function initialize($paymentAction, $stateObject) {
         //chama model para fazer o post do pagamento
 		$response = Mage::getModel('mercadopago/custom_payment')->postPago();
-	
+
         if($response !== false):
             $this->getInfoInstance()->setAdditionalInformation('activation_uri', $response['response']['activation_uri']);
             return true;
         endif;
-        
+
         return false;
-        
+
     }
 
 	public function assignData($data){
-	
+
 		// route /checkout/onepage/savePayment
 		if(!($data instanceof Varien_Object)){
 			$data = new Varien_Object($data);
 		}
-		
+
 		//get array info
 		$info_form = $data->getData();
-		
+
 		$info = $this->getInfoInstance();
 		$info->setAdditionalInformation('payment_type_id', "ticket");
 		$info->setAdditionalInformation('payment_method', $info_form['payment_method_boleto']);
@@ -65,19 +65,19 @@ class MercadoPago_Model_CustomTicket_Payment extends Mage_Payment_Model_Method_A
 		$info->setAdditionalInformation('issuers', "");
 		$info->setAdditionalInformation('installments', 1);
 		$info->setAdditionalInformation('doc_number', "");
-		
-		if(isset($info_form['coupon_code'])){
-			$info->setAdditionalInformation('coupon_code', $info_form['coupon_code']);
+
+		if(isset($info_form['mercadopago_customticket']['coupon_code'])){
+			$info->setAdditionalInformation('coupon_code', $info_form['mercadopago_customticket']['coupon_code']);
 		}
-		
+
 		return $this;
 	}
-    
+
 	public function getOrderPlaceRedirectUrl() {
 		// requisicao vem da pagina de finalizacao de pedido
 		return Mage::getUrl('mercadopago/success', array('_secure' => true));
 	}
-	
+
 }
 
 ?>

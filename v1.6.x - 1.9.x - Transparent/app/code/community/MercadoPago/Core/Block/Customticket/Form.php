c56cf6c0317ddaf5efae9cc275afcbff810636d0
Damian Achaga <dachaga@summasolutions.net>
2015-12-18 18:13:18 -0300

    desambiguous slectors from custom and custom ticket in mercadopago.js

--- v1.6.x - 1.9.x - Transparent/app/code/community/MercadoPago/Core/Block/Customticket/Form.php
+++ v1.6.x - 1.9.x - Transparent/app/code/community/MercadoPago/Core/Block/Customticket/Form.php
@@ -16,41 +16,13 @@
 
 
 class MercadoPago_Core_Block_Customticket_Form
-    extends Mage_Payment_Block_Form_Cc
+    extends MercadoPago_Core_Block_AbstractForm
 {
     protected function _construct()
     {
         parent::_construct();
 
         $this->setTemplate('mercadopago/custom_ticket/form.phtml');
-    }
-
-    protected function _prepareLayout()
-    {
-
-        //pega public key para settar no aquivo mercadopago.js
-        $public_key = Mage::getStoreConfig(MercadoPago_Core_Helper_Data::XML_PATH_PUBLIC_KEY);
-
-        //init js no header
-        $block = Mage::app()->getLayout()->createBlock('core/text', 'js_mercadopago');
-        $block->setText(
-            sprintf(
-                '
-                <script type="text/javascript">var PublicKeyMercadoPagoCustom = "' . $public_key .'";</script>
-                <script src="https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js"></script>
-                <script type="text/javascript" src="%s"></script>',
-                Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_JS, true) . 'mercadopago/mercadopago.js',
-                Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_JS, true) . 'mercadopago/tinyJ.js'
-            )
-        );
-
-        $head = Mage::app()->getLayout()->getBlock('after_body_start');
-
-        if ($head) {
-            $head->append($block);
-        }
-
-        return parent::_prepareLayout();
     }
 
     public function getTicketsOptions()

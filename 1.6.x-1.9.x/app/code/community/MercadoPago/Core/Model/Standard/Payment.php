8b6c886a933c4ad60b69507becde0f041b50d75e
damibosrengo <damibosrengo@gmail.com>
2016-03-21 11:04:31 -0300

    add param ME request

--- 1.6.x-1.9.x/app/code/community/MercadoPago/Core/Model/Standard/Payment.php
+++ 1.6.x-1.9.x/app/code/community/MercadoPago/Core/Model/Standard/Payment.php
@@ -257,6 +257,7 @@ class MercadoPago_Core_Model_Standard_Payment
         if (empty($paramsShipment)) {
             $paramsShipment = $params->getData();
             $paramsShipment['cost'] = (float)$order->getBaseShippingAmount();
+            $paramsShipment['mode'] = 'custom';
         }
         $paramsShipment['receiver_address'] = $this->getReceiverAddress($shippingAddress);
         return $paramsShipment;

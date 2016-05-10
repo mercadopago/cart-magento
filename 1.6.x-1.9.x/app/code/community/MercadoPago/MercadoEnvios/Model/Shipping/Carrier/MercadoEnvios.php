d7ead74ec3aac54588895b59814cec88fd890445
imasson <imasson@summasolutions.net>
2016-05-10 14:22:15 -0300

    #637 - [support] Error en la suma de las dimensiones - alto - ancho y longitud #117

--- 1.6.x-1.9.x/app/code/community/MercadoPago/MercadoEnvios/Model/Shipping/Carrier/MercadoEnvios.php
+++ 1.6.x-1.9.x/app/code/community/MercadoPago/MercadoEnvios/Model/Shipping/Carrier/MercadoEnvios.php
@@ -18,6 +18,7 @@ class MercadoPago_MercadoEnvios_Model_Shipping_Carrier_MercadoEnvios
     protected $_methods;
     protected $_request;
 
+
     /**
      * Collect and get rates
      *
@@ -88,9 +89,9 @@ class MercadoPago_MercadoEnvios_Model_Shipping_Carrier_MercadoEnvios
                 return;
             }
 
-            $client_id = Mage::getStoreConfig(MercadoPago_Core_Helper_Data::XML_PATH_CLIENT_ID);
-            $client_secret = Mage::getStoreConfig(MercadoPago_Core_Helper_Data::XML_PATH_CLIENT_SECRET);
-            $mp = Mage::helper('mercadopago')->getApiInstance($client_id, $client_secret);
+            $clientId = Mage::getStoreConfig(MercadoPago_Core_Helper_Data::XML_PATH_CLIENT_ID);
+            $clientSecret = Mage::getStoreConfig(MercadoPago_Core_Helper_Data::XML_PATH_CLIENT_SECRET);
+            $mp = Mage::helper('mercadopago')->getApiInstance($clientId, $clientSecret);
 
             $params = [
                 "dimensions" => $dimensions,

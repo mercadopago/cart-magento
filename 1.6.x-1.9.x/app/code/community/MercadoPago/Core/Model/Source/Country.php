6678fc1de37093d916c7542b8924f41bf0129472
imasson <imasson@summasolutions.net>
2016-05-31 18:16:06 -0300

    Coding standard

--- 1.6.x-1.9.x/app/code/community/MercadoPago/Core/Model/Source/Country.php
+++ 1.6.x-1.9.x/app/code/community/MercadoPago/Core/Model/Source/Country.php
@@ -21,12 +21,12 @@ class MercadoPago_Core_Model_Source_Country
     {
         $country = [];
         $country[] = ['value' => "mla", 'label' => Mage::helper('mercadopago')->__("Argentina"), 'code' => 'AR'];
-        $country[] = array('value' => "mlb", 'label' => Mage::helper('mercadopago')->__("Brasil"), 'code' => 'BR');
-        $country[] = array('value' => "mco", 'label' => Mage::helper('mercadopago')->__("Colombia"), 'code' => 'CO');
-        $country[] = array('value' => "mlm", 'label' => Mage::helper('mercadopago')->__("Mexico"), 'code' => 'MX');
-        $country[] = array('value' => "mlc", 'label' => Mage::helper('mercadopago')->__("Chile"), 'code' => 'CL');
-        $country[] = array('value' => "mlv", 'label' => Mage::helper('mercadopago')->__("Venezuela"), 'code' => 'VE');
-        $country[] = array('value' => "mpe", 'label' => Mage::helper('mercadopago')->__("Perú"), 'code' => 'PE');
+        $country[] = ['value' => "mlb", 'label' => Mage::helper('mercadopago')->__("Brasil"), 'code' => 'BR'];
+        $country[] = ['value' => "mco", 'label' => Mage::helper('mercadopago')->__("Colombia"), 'code' => 'CO'];
+        $country[] = ['value' => "mlm", 'label' => Mage::helper('mercadopago')->__("Mexico"), 'code' => 'MX'];
+        $country[] = ['value' => "mlc", 'label' => Mage::helper('mercadopago')->__("Chile"), 'code' => 'CL'];
+        $country[] = ['value' => "mlv", 'label' => Mage::helper('mercadopago')->__("Venezuela"), 'code' => 'VE'];
+        $country[] = ['value' => "mpe", 'label' => Mage::helper('mercadopago')->__("Perú"), 'code' => 'PE'];
 
         //force order by key
         ksort($country);

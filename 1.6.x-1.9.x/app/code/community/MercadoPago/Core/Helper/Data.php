c94f60af05b7eb78ecbf8048e4d66194d4e3654e
imasson <imasson@summasolutions.net>
2016-06-02 16:13:25 -0300

    Financing Cost fix

--- 1.6.x-1.9.x/app/code/community/MercadoPago/Core/Helper/Data.php
+++ 1.6.x-1.9.x/app/code/community/MercadoPago/Core/Helper/Data.php
@@ -208,6 +208,12 @@ class MercadoPago_Core_Helper_Data
             $balance = $balance - $transactionAmount - $shippingCost;
         }
 
+        if (!Mage::getStoreConfigFlag('payment/mercadopago/financing_cost')) {
+            $order->setGrandTotal($order->getGrandTotal() - $balance);
+            $order->setBaseGrandTotal($order->getBaseGrandTotal() - $balance);
+            return;
+        }
+
         if (Zend_Locale_Math::round($balance, 4) > 0) {
             $order->setFinanceCostAmount($balance);
             $order->setBaseFinanceCostAmount($balance);
